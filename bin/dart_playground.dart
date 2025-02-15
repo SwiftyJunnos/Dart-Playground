4233edad2de5ddd6a96e1c5f08adaf536f7dd557
Junnos  <junnos.dev@gmail.com>
2025-02-16 00:23:07 +0900

    Initial commit

---
+++ bin/dart_playground.dart
@@ -0,0 +1,5 @@
+import 'package:dart_playground/dart_playground.dart' as dart_playground;
+
+void main(List<String> arguments) {
+  dart_playground.main(arguments);
+}
---
+++ test/dart_playground_test.dart
@@ -0,0 +1,4 @@
+import 'package:dart_playground/dart_playground.dart';
+import 'package:test/test.dart';
+
+void main() {}
