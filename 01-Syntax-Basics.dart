624dd97e547d6132f94d3ea12ce1fc22ff8bf329
Junnos  <junnos.dev@gmail.com>
2025-02-16 00:56:25 +0900

    01-Syntax-Basics_libraries

--- 01-Syntax-Basics.dart
+++ 01-Syntax-Basics.dart
@@ -1,9 +1,11 @@
 import 'dart:math';
+import 'dart:collection' deferred as collection show HashMap;
 
 void main() {
   // variables();
   // operators();
-  metadata();
+  // metadata();
+  libraries();
 }
 
 void variables() {
@@ -114,4 +116,24 @@ void doSomething() {
 
 void metadata() {
   doSomething();
+}
+
+// Lazy loading library
+// import 'dart:collection' deferred as collection show HashMap;
+
+Future<void> libraries() async {
+  // Load when lazy library is needed
+  await collection.loadLibrary();
+  var hashmap = collection.HashMap<String, int>();
+  hashmap['k1'] = 1;
+  print(hashmap);
+
+  // 라이브러리를 deferred(lazy) loading 할 때 유의할 점
+  // 1. 라이브러리의 상수(constant)는 import한 파일에서는 상수가 아니다.
+  //    loadLibrary()를 호출하기 전까지는 값이 존재하지 않기 때문이다.
+  //    따라서 const 값을 가져오더라도, 그 값은 실제로 const가 아니다.
+  // 2. 라이브러리의 타입을 사용할 수 없다.
+  //    이는 공유 인터페이스를 별도의 라이브러리로 분리하여 사용하는 것으로 해결할 수 있다.
+  // 3. loadLibrary() 함수는 `deferred as namespace`를 사용할 때 Dart 언어 내부적으로 추가해주는 기능이다.
+  //    loadLibrary() 함수는 `Future` 값을 반환한다.
 }
