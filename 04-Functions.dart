82892a93dbee8c25011fd9bec903481bee2dbccd
Junnos  <junnos.dev@gmail.com>
2025-02-17 00:58:16 +0900

    04-Functions-functions

---
+++ 04-Functions.dart
@@ -0,0 +1,41 @@
+void main() async {
+  functions();
+}
+
+void enableFlag(bool bold) {}
+
+// Named parameter는 {} 안에서 사용
+// Named parameter: 기본값 파라미터
+// 기본값을 명시하지 않으면 Null로 설정됨 (bool?과 같이 타입 명시 필요)
+// 명시하지 않고 Nullable로 사용하지 않으려면 required 사용
+void enableFlags({bool bold = false, required bool hidden}) {
+  print('bold: $bold, hidden: $hidden');
+}
+
+String say(String from, String msg, [String? device]) {
+  var result = '$from says $msg';
+  if (device != null) {
+    result = '$result with a $device';
+  }
+  return result;
+}
+
+Function makeAdder(int addBy) {
+  return (int i) => addBy + i;
+}
+
+void functions() {
+  enableFlags(bold: true, hidden: false);
+  assert(say('John', 'Hello') == 'John says Hello');
+  assert(say('John', 'Hello', 'phone') == 'John says Hello with a phone');
+
+  // Anonymous functions
+  List<String> list = ['a', 'b', 'c'];
+  var uppercaseList = list.map((item) => item.toUpperCase()).toList();
+  print(uppercaseList);
+
+  // Lexical closure
+  var add2 = makeAdder(2);
+  print(add2(3));
+}
+
